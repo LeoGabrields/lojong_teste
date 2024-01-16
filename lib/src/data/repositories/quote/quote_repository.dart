7cb650ed7895c8e366d35e2e4e7c331664dba498
Leandro Gabriel <leogabrields97@gmail.com>
2024-01-16 16:46:17 -0300

    removing

--- lib/src/data/repositories/quote/quote_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:lojong_teste/src/core/exceptions/repository_exception.dart';
-import 'package:lojong_teste/src/core/fp/either.dart';
-import 'package:lojong_teste/src/models/quotes/quote_model.dart';
-
-abstract interface class QuoteRepository {
-  Future<Either<RepositoryException, List<QuoteModel>>> fetchQuotesList();
-}
--- lib/src/data/repositories/video/video_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:lojong_teste/src/core/exceptions/repository_exception.dart';
-import 'package:lojong_teste/src/core/fp/either.dart';
-import 'package:lojong_teste/src/models/video/video_model.dart';
-
-abstract interface class VideoRepository {
-  Future<Either<RepositoryException, List<VideoModel>>> fetchVideosList();
-}
