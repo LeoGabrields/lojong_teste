f2c8feaebc74de6fc6dcb10c26bfe6b104c2e7c4
Leandro Gabriel <leogabrields97@gmail.com>
2024-01-16 16:44:34 -0300

    implementing DioCache and chunckerDioInterceptor

--- lib/src/core/rest_client/rest_client.dart
+++ lib/src/core/rest_client/rest_client.dart
@@ -1,5 +1,10 @@
+import 'package:chucker_flutter/chucker_flutter.dart';
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+import 'package:dio_cache_interceptor_file_store/dio_cache_interceptor_file_store.dart';
+import 'package:flutter/foundation.dart';
+import 'package:lojong_teste/src/core/path_provider/app_path_provider.dart';
 
 final class RestClient extends DioForNative {
   RestClient()
@@ -10,7 +15,21 @@ final class RestClient extends DioForNative {
               'Authorization':
                   'Bearer O7Kw5E2embxod5YtL1h1YsGNN7FFN8wIxPYMg6J9zFjE6Th9oDssEsFLVhxf'
             },
-            connectTimeout: const Duration(seconds: 10),
           ),
-        );
+        ) {
+    interceptors.add(
+      DioCacheInterceptor(
+        options: CacheOptions(
+          store: FileCacheStore(AppPathProvider.path),
+          policy: CachePolicy.refreshForceCache,
+          hitCacheOnErrorExcept: [401, 404],
+          maxStale: const Duration(days: 7),
+          priority: CachePriority.high,
+        ),
+      ),
+    );
+    if (kDebugMode) {
+      interceptors.add(ChuckerDioInterceptor());
+    }
+  }
 }
