a709a19226e5285fff6956275eafa8d6a24f1666
Leandro Gabriel <leogabrields97@gmail.com>
2024-01-16 16:45:25 -0300

    initializing path provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:lojong_teste/src/core/path_provider/app_path_provider.dart';
 import 'package:lojong_teste/src/lojong_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppPathProvider.initPath();
+
   runApp(const LojongApp());
 }
