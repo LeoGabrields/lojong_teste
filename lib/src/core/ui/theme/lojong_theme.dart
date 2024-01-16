970c571a8745b8e0de7f1f34984413bb5706428e
Leandro Gabriel <leogabrields97@gmail.com>
2024-01-16 16:35:53 -0300

    added themeData

--- lib/src/core/ui/theme/lojong_theme.dart
+++ lib/src/core/ui/theme/lojong_theme.dart
@@ -3,27 +3,26 @@ import 'package:flutter/material.dart';
 import '../styles/colors_app.dart';
 import '../styles/text_styles.dart';
 
-sealed class LojongTheme {
-  static ThemeData themeData = ThemeData(
-    scaffoldBackgroundColor: ColorsApp.instance.primary,
-    appBarTheme: AppBarTheme(
-      centerTitle: true,
-      titleTextStyle: TextStyles.instance.textTitle.copyWith(fontSize: 15),
-      color: ColorsApp.instance.primary,
+ThemeData themeData = ThemeData(
+  scaffoldBackgroundColor: ColorsApp.instance.primary,
+  fontFamily: 'Asap',
+  appBarTheme: AppBarTheme(
+    centerTitle: true,
+    titleTextStyle: TextStyles.instance.textTitle.copyWith(fontSize: 15),
+    color: ColorsApp.instance.primary,
+  ),
+  tabBarTheme: TabBarTheme(
+    indicatorSize: TabBarIndicatorSize.tab,
+    splashFactory: NoSplash.splashFactory,
+    indicatorColor: Colors.transparent,
+    dividerColor: Colors.transparent,
+    unselectedLabelStyle: TextStyles.instance.textSemiBold,
+    labelStyle: TextStyles.instance.textSemiBold,
+    labelColor: const Color(0xFFBF7964),
+    unselectedLabelColor: Colors.white,
+    indicator: BoxDecoration(
+      borderRadius: BorderRadius.circular(55),
+      color: Colors.white,
     ),
-    tabBarTheme: TabBarTheme(
-      indicatorSize: TabBarIndicatorSize.tab,
-      splashFactory: NoSplash.splashFactory,
-      indicatorColor: Colors.transparent,
-      dividerColor: Colors.transparent,
-      unselectedLabelStyle: TextStyles.instance.textSemiBold,
-      labelStyle: TextStyles.instance.textSemiBold,
-      labelColor: const Color(0xFFBF7964),
-      unselectedLabelColor: Colors.white,
-      indicator: BoxDecoration(
-        borderRadius: BorderRadius.circular(55),
-        color: Colors.white,
-      ),
-    ),
-  );
-}
+  ),
+);
--- lib/src/lojong_app.dart
+++ lib/src/lojong_app.dart
@@ -1,8 +1,10 @@
+import 'package:chucker_flutter/chucker_flutter.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:lojong_teste/src/core/provider/application_binding.dart';
 import 'package:lojong_teste/src/core/ui/theme/lojong_theme.dart';
-import 'package:lojong_teste/src/view/inspiration_view.dart';
+import 'package:lojong_teste/src/features/inspiration/inspiration_view.dart';
 
 class LojongApp extends StatelessWidget {
   const LojongApp({super.key});
@@ -11,9 +13,11 @@ class LojongApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ApplicationBinding(
       child: MaterialApp(
+        navigatorObservers:
+            kDebugMode ? [ChuckerFlutter.navigatorObserver] : [],
         debugShowCheckedModeBanner: false,
         title: 'Lojong App',
-        theme: LojongTheme.themeData,
+        theme: themeData,
         home: const InspirationView(),
       ),
     );
